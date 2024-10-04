ca82b7a8032d784df30008e33e7470387b1b0380
Raj Garande <18751312+rajxd@users.noreply.github.com>
2024-10-04 22:00:37 +0530

    exposed micrometer on port 8887

--- src/main/java/com/raj/app/vertex_grafana_prometheus/App.java
+++ src/main/java/com/raj/app/vertex_grafana_prometheus/App.java
@@ -1,12 +1,25 @@
 package com.raj.app.vertex_grafana_prometheus;
 
+import java.util.EnumSet;
+
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.micrometer.Label;
+import io.vertx.micrometer.MicrometerMetricsOptions;
+import io.vertx.micrometer.VertxPrometheusOptions;
 
 public class App {
 
 	public static void main(final String... args) {
-	    Vertx vertx = Vertx.vertx();
-	    vertx.deployVerticle(new WebServerVerticle());
+
+		MicrometerMetricsOptions options = new MicrometerMetricsOptions()
+				.setPrometheusOptions(new VertxPrometheusOptions().setStartEmbeddedServer(true)
+						.setEmbeddedServerOptions(new HttpServerOptions().setPort(8887)).setEnabled(true))
+				.setLabels(EnumSet.allOf(Label.class)).setEnabled(true);
+
+	    Vertx vertx = Vertx.vertx(new VertxOptions().setMetricsOptions(options));
+		vertx.deployVerticle(new WebServerVerticle());
 	}
 
 }
