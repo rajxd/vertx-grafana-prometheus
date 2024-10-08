5a6a18d80b3b832833b76f789c34daad5632c0f9
Raj Garande <18751312+rajxd@users.noreply.github.com>
2024-10-08 23:08:43 +0530

    added eventBusConsumerVerticle

---
+++ src/main/java/com/raj/app/vertex_grafana_prometheus/EventBusConsumerVerticle.java
@@ -0,0 +1,18 @@
+package com.raj.app.vertex_grafana_prometheus;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.example.micrometer.verticles.Greetings;
+import io.vertx.ext.web.Router;
+
+public class EventBusConsumerVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(Promise<Void> startPromise) throws Exception {
+
+		vertx.eventBus().consumer("greetings", x->{
+			x.reply("Hello from Eventbus");
+		});
+	}
+}
